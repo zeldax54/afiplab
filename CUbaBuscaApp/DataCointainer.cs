b3ee8faf3d4a3fbbc6a133cbb8edecba5e946e1b
zeldax <zeldax54@gmail.com>
2019-04-17 01:58:48 -0300

    anotando singelton

--- CUbaBuscaApp/DataCointainer.cs
+++ CUbaBuscaApp/DataCointainer.cs
@@ -9,22 +9,21 @@ namespace CUbaBuscaApp
 {
     class DataContainer
     {
+        //Singleton para nunca olvidar
+      /*  En ingeniería de software, singleton o instancia única es un patrón de diseño que permite restringir
+        la creación de objetos pertenecientes a una clase o el valor de un tipo a un único objeto.
+     Su intención consiste en garantizar que una clase solo tenga una instancia y proporcionar un punto de 
+     acceso global a ella.El patrón singleton se implementa creando en nuestra clase un método que crea una 
+     instancia del objeto solo si todavía no existe alguna.Para asegurar que la clase no puede ser instanciada 
+     nuevamente se regula el alcance del constructor(con modificadores de acceso como protegido o privado). */
+
         private static DataContainer oInstance;
 
 
         public BdManager dbManager;
-
-
-
-
-
         protected DataContainer()
         {
-
         }
-
-
-
 
         public static DataContainer Instance()
         {
